4648736503f1d7cefc6fa42afddb485fb198d6b0
vitordino <me@vitordino.com>
2023-04-14 04:10:04 -0300

    :truck: put back app

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from 'renderer/App'
-import useStore from 'renderer/hooks/useStore'
+import App from 'renderer/App'
 import Counter from './components/Counter/Counter'
 
 const container = document.getElementById('root')!
@@ -9,6 +8,6 @@ const root = createRoot(container)
 root.render(
 	<StrictMode>
 		<Counter />
-		{/* <App /> */}
+		<App />
 	</StrictMode>,
 )
