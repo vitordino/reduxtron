980bd5cb03f174fd43e4237623bdf09431a6ac08
vitordino <me@vitordino.com>
2023-04-17 03:04:21 -0300

    :sparkles: add checkbox component

---
+++ src/renderer/components/Checkbox.tsx
@@ -0,0 +1,18 @@
+import type { DetailedHTMLProps, InputHTMLAttributes } from 'react'
+import cn from 'clsx'
+
+type CheckboxProps = Omit<
+	DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+	'type'
+>
+
+export const Checkbox = ({ className, ...props }: CheckboxProps) => (
+	<input
+		{...props}
+		type='checkbox'
+		className={cn(
+			'rounded-sm appearance-none form-checkbox focus:ring-offset-slate-2 focus:ring-0 focus:outline-none focus-visible:ring-offset-slate-2 focus-visible:ring-2 focus-visible:ring-indigo-8 form-stick bg-indigo-1 checked:bg-indigo-7 checked:border-transparent border border-slate-6 md:border-transparent hover:border-slate-7 w-3.5 h-3.5 cursor-pointer',
+			className,
+		)}
+	/>
+)
