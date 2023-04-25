a24db2ef54e2336fe5af62423d27e4179ad54166
vitordino <me@vitordino.com>
2023-04-25 01:42:31 -0300

    :lipstick: move finder select to toolbar

--- src/renderer/components/AppLayout.tsx
+++ src/renderer/components/AppLayout.tsx
@@ -5,7 +5,7 @@ const AppLayout = () => (
 	<>
 		<Sidebar />
 		<div className='fixed ml-20 lg:ml-56 w-screen min-h-screen bg-slate-1 dark:bg-slate-2 shadow-slate-8 dark:shadow-slate-2 shadow-xl' />
-		<main className='relative ml-20 lg:ml-56 w-full min-h-screen flex flex-col'>
+		<main className='relative ml-20 lg:ml-56 w-full min-h-screen flex flex-col overflow-hidden'>
 			<div className='h-9' />
 			<Outlet />
 		</main>
--- src/renderer/components/Finder.tsx
+++ src/renderer/components/Finder.tsx
@@ -18,6 +18,8 @@ const Finder = () => {
 		return { ...acc, [curr]: arr.slice(0, index + 1).join('/') }
 	}, {})
 
+	const hasPermutations = Object.keys(permutations || {}).length > 1
+
 	const handleSelectChange = e => dispatch({ type: 'FOLDER:SET', payload: e.target.value })
 
 	const moveUp = () => {
@@ -35,7 +37,22 @@ const Finder = () => {
 
 	return (
 		<>
-			<Toolbar>finder</Toolbar>
+			<Toolbar>
+				{hasPermutations && (
+					<select
+						value={path}
+						onChange={handleSelectChange}
+						className='no-drag-region h-full focus:outline-none focus:bg-slate-5 ring-indigo-8 pl-3 pr-4 -ml-4'
+					>
+						{Object.entries(permutations || {}).map(([key, value]) => (
+							<option key={value} value={value}>
+								{key}
+							</option>
+						))}
+					</select>
+				)}
+				{!hasPermutations && 'finder'}
+			</Toolbar>
 			<Button
 				onClick={() => dispatch({ type: 'FOLDER:PICK' })}
 				type='button'
@@ -59,15 +76,7 @@ const Finder = () => {
 			>
 				<RxChevronUp />
 			</Button>
-			{permutations && (
-				<select value={path} onChange={handleSelectChange}>
-					{Object.entries(permutations).map(([key, value]) => (
-						<option key={value} value={value}>
-							{key}
-						</option>
-					))}
-				</select>
-			)}
+
 			<pre>{JSON.stringify({ path, permutations }, null, 2)}</pre>
 			<NavigationMenu.Root>
 				<NavigationMenu.List>
