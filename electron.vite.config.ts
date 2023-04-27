b3fe88b352086b7740c327af19c3330ac361273f
vitordino <me@vitordino.com>
2023-04-27 09:01:31 -0300

    :bug: fix native-prompt on production

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,16 +2,21 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const optimizeDeps = { exclude: ['native-prompt'] }
+
 export default defineConfig({
 	main: {
 		plugins: [tsconfigPaths(), externalizeDepsPlugin()],
+		optimizeDeps,
 	},
 	preload: {
 		plugins: [tsconfigPaths(), externalizeDepsPlugin()],
+		optimizeDeps,
 	},
 	renderer: {
 		plugins: [tsconfigPaths(), react()],
 		publicDir: '../../resources',
+		optimizeDeps,
 		define: {
 			__PLATFORM__: JSON.stringify(process.platform),
 		},
