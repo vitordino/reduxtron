7cf26402e94bc101b985a83970d7cc092bdbb95c
vitordino <me@vitordino.com>
2023-04-27 03:54:12 -0300

    :sparkles: add new to do action to tray

--- src/main/tray/ToDo.ts
+++ src/main/tray/ToDo.ts
@@ -1,18 +1,40 @@
 import type { MenuItemConstructorOptions } from 'electron'
+import nativePrompt from 'native-prompt'
 import type { Dispatch, State } from 'shared/reducers'
+import { ToDo } from 'shared/reducers/toDos'
 
 const toggleToDo = (dispatch: Dispatch, id: string) => () =>
 	dispatch({ type: 'TO_DO:TOGGLE', payload: id })
 
-const TrayToDo = (state: Partial<State>, dispatch: Dispatch): MenuItemConstructorOptions => ({
-	label: 'to do',
-	type: 'submenu',
-	submenu: (state.toDos?.items || [])?.map(({ id, title, completed }) => ({
+const Item =
+	(dispatch: Dispatch) =>
+	({ id, title, completed }: ToDo): MenuItemConstructorOptions => ({
 		label: title,
 		type: 'checkbox',
 		checked: completed,
 		click: toggleToDo(dispatch, id),
-	})),
-})
+	})
+
+const addToDo = (state: Partial<State>, dispatch: Dispatch) => async () => {
+	const defaultText = state.toDos?.draft
+	const payload = await nativePrompt('create to do', 'fill with content', { defaultText })
+	if (!payload) return
+	dispatch({ type: 'TO_DO:CREATE', payload })
+	dispatch({ type: 'TO_DO:SET_DRAFT', payload: '' })
+}
+
+const TrayToDo = (state: Partial<State>, dispatch: Dispatch): MenuItemConstructorOptions => {
+	const items = state?.toDos?.items || []
+
+	return {
+		label: 'to do',
+		type: 'submenu',
+		submenu: [
+			{ label: 'add to do', type: 'normal', click: addToDo(state, dispatch) },
+			{ type: 'separator' },
+			...items.map(Item(dispatch)),
+		],
+	}
+}
 
 export default TrayToDo
