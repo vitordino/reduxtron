aef1c133e0ba9f1b20e5ce0e852204429fd1609a
vitordino <me@vitordino.com>
2023-04-29 11:20:12 -0300

    :lipstick: sidebar hover/active bg using opacity

--- src/renderer/components/Sidebar.tsx
+++ src/renderer/components/Sidebar.tsx
@@ -47,15 +47,40 @@ type SidebarLinkProps = {
 	position: 'top' | 'bottom'
 }
 
+const LogoLink = () => {
+	const pathname = useLocation().pathname
+	return (
+		<NavigationMenu.Item>
+			<NavigationMenu.Link asChild>
+				<Link
+					to='/'
+					data-current={pathname === '/'}
+					className='group flex items-center justify-between rounded hover:bg-slate-1 hover:bg-opacity-30 data-[current=true]:bg-slate-1 data-[current=true]:bg-opacity-50 hover:data-[current=true]:bg-opacity-60'
+					{...keyboardLogoHandler}
+				>
+					<div className='flex items-center p-2'>
+						<div className='flex text-sm items-center justify-center rounded-sm w-6 h-6 p-0.5 text-white bg-indigo-9 group-hover:bg-indigo-10 lg:x2.5'>
+							<img src='images/logo.svg' />
+						</div>
+						<div className='group-hover:text-slate-12 text-sm font-medium hidden lg:block ml-2'>
+							redux-electron
+						</div>
+					</div>
+				</Link>
+			</NavigationMenu.Link>
+		</NavigationMenu.Item>
+	)
+}
+
 const SidebarLink = ({ to, icon: Icon, text, position }: SidebarLinkProps) => {
-	const location = useLocation().pathname
+	const pathname = useLocation().pathname
 	return (
 		<NavigationMenu.Item className='lg:px-4'>
 			<NavigationMenu.Link asChild {...KEYBOARD_HANDLERS[position]}>
 				<Link
 					to={to}
-					data-current={location === to}
-					className='relative group lg:w-full mt-0.5 py-2 px-2 h-7 flex items-center justify-center lg:justify-normal lg:rounded hover:bg-slate-6 cursor-pointer data-[current=true]:bg-slate-2'
+					data-current={pathname === to}
+					className='relative group lg:w-full mt-0.5 py-2 px-2 h-7 flex items-center justify-center lg:justify-normal lg:rounded cursor-pointer hover:bg-slate-1 hover:bg-opacity-30 data-[current=true]:bg-slate-1 data-[current=true]:bg-opacity-50 hover:data-[current=true]:bg-opacity-60'
 				>
 					<Icon className='icon-size text-sm text-slate-11 group-hover:text-slate-12' />
 					<span className='hidden lg:block ml-2 text-slate-12'>{text}</span>
@@ -71,24 +96,7 @@ export const Sidebar = () => (
 			<ul className='list-none inset-0 fixed flex flex-col flex-shrink-0 w-20 lg:w-56 font-sans text-sm border-r border-slate-6 dark:border-slate-1 justify-items-start z-100'>
 				<div className='drag-region sticky top-0 flex-shrink-0 w-full h-9' />
 				<div className='flex flex-col flex-grow-0 flex-shrink-0 py-1 px-5 lg:px-4'>
-					<NavigationMenu.Item>
-						<NavigationMenu.Link asChild>
-							<Link
-								to='/'
-								className='group flex items-center justify-between rounded hover:bg-slate-6'
-								{...keyboardLogoHandler}
-							>
-								<div className='flex items-center p-2'>
-									<div className='flex text-sm items-center justify-center rounded-sm w-6 h-6 p-0.5 text-white bg-indigo-9 group-hover:bg-indigo-10 lg:x2.5'>
-										<img src='images/logo.svg' />
-									</div>
-									<div className='group-hover:text-slate-12 text-sm font-medium hidden lg:block ml-2'>
-										redux-electron
-									</div>
-								</div>
-							</Link>
-						</NavigationMenu.Link>
-					</NavigationMenu.Item>
+					<LogoLink />
 				</div>
 
 				<div className='flex flex-col flex-shrink flex-grow overflow-y-auto lg:pt-2'>
