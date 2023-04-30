e24ad50ff7b4987ff56036a19ece423eac5783f7
vitordino <me@vitordino.com>
2023-04-30 19:30:00 -0300

    :fire: remove native-prompt

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,21 +2,12 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const optimizeDeps = { exclude: ['native-prompt'] }
-
 export default defineConfig({
-	main: {
-		plugins: [tsconfigPaths(), externalizeDepsPlugin()],
-		optimizeDeps,
-	},
-	preload: {
-		plugins: [tsconfigPaths(), externalizeDepsPlugin()],
-		optimizeDeps,
-	},
+	main: { plugins: [tsconfigPaths(), externalizeDepsPlugin()] },
+	preload: { plugins: [tsconfigPaths(), externalizeDepsPlugin()] },
 	renderer: {
 		plugins: [tsconfigPaths(), react()],
 		publicDir: '../../resources',
-		optimizeDeps,
 		define: {
 			__PLATFORM__: JSON.stringify(process.platform),
 		},
