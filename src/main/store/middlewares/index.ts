e41220e290adb11398a5122c143567d568fd519d
vitordino <me@vitordino.com>
2023-04-30 21:37:03 -0300

    :wrench: add persistance, remove logger middleware

--- src/main/store/middlewares/index.ts
+++ src/main/store/middlewares/index.ts
@@ -2,16 +2,14 @@ import thunk from 'redux-thunk'
 
 import { Middleware } from 'shared/reducers'
 import { folderMiddleware } from 'main/store/middlewares/folder'
-// import { persistanceMiddleware } from 'main/store/middlewares/persistance'
+import { persistanceMiddleware } from 'main/store/middlewares/persistance'
 import { swrMiddleware } from 'main/store/middlewares/swr'
 import { uiMiddleware } from 'main/store/middlewares/ui'
-import { logger } from './logger'
 
 export const middleware: Middleware[] = [
 	thunk,
 	swrMiddleware,
 	folderMiddleware,
-	logger,
-	// persistanceMiddleware,
+	persistanceMiddleware,
 	uiMiddleware,
 ]
