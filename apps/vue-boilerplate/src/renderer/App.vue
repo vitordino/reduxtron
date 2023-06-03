1c2af1d64c635b1fdaecca7c9ef093ad9cb9bd30
vitordino <me@vitordino.com>
2023-06-03 02:42:29 -0300

    :bug: get initial state on vue boilerplate

--- apps/vue-boilerplate/src/renderer/App.vue
+++ apps/vue-boilerplate/src/renderer/App.vue
@@ -1,27 +1,36 @@
 <script lang="ts" setup>
-  import { ref, onMounted } from 'vue'
+import { State } from 'shared/reducers'
+import { ref, onBeforeMount } from 'vue'
 
-  const counter = ref(0)
+const counter = ref<number | undefined>()
 
-  const dispatch = window.electron.dispatch
-  const onDecrement = () => dispatch({ type: 'COUNTER:DECREMENT' })
-  const onIncrement = () => dispatch({ type: 'COUNTER:INCREMENT' })
+const dispatch = window.electron.dispatch
+const onDecrement = () => dispatch({ type: 'COUNTER:DECREMENT' })
+const onIncrement = () => dispatch({ type: 'COUNTER:INCREMENT' })
 
-  onMounted(() =>
-    window.electron.subscribe(x => {
-      counter.value = x.counter
-    })
-  )
+onBeforeMount(() => {
+	const setCounter = (state: Partial<State>) => {
+		counter.value = state.counter
+	}
+	window.electron.getState().then(setCounter)
+	window.electron.subscribe(setCounter)
+})
 </script>
 
 <template>
-  <main className='h-screen flex flex-col items-center justify-center'>
-    <button className='border-gray-800 border-2 rounded px-4 py-1 hover:bg-gray-800 hover:text-white focus-visible:bg-gray-800 focus-visible:text-white' @click="onDecrement">
-      decrement
-    </button>
-    <pre>{{counter}}</pre>
-    <button className='border-gray-800 border-2 rounded px-4 py-1 hover:bg-gray-800 hover:text-white focus-visible:bg-gray-800 focus-visible:text-white' @click="onIncrement">
-      increment
-    </button>
-  </main>
+	<main className="h-screen flex flex-col items-center justify-center">
+		<button
+			className="border-gray-800 border-2 rounded px-4 py-1 hover:bg-gray-800 hover:text-white focus-visible:bg-gray-800 focus-visible:text-white"
+			@click="onDecrement"
+		>
+			decrement
+		</button>
+		<pre>{{ counter ?? 'loading' }}</pre>
+		<button
+			className="border-gray-800 border-2 rounded px-4 py-1 hover:bg-gray-800 hover:text-white focus-visible:bg-gray-800 focus-visible:text-white"
+			@click="onIncrement"
+		>
+			increment
+		</button>
+	</main>
 </template>
