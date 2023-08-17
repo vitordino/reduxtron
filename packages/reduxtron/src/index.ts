065e08b44a22b28a633539db39fc9b21870f69be
vitordino <me@vitordino.com>
2023-08-17 00:33:18 -0300

    :fire: remove lib index.ts because it exports mixed node + browser + preload specifics

--- packages/reduxtron/src/index.ts
+++
@@ -1,5 +0,0 @@
-export type { MainReduxBridge } from "./main";
-export { mainReduxBridge } from "./main";
-export type { PreloadReduxBridge, PreloadReduxBridgeReturn } from "./preload";
-export { preloadReduxBridge } from "./preload";
-export { createUseStore } from "./zustand-store";
